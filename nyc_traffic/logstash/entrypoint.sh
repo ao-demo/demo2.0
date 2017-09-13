db573d9a9b06c5b61c85cb7ff7894f168065b364
rajkrishnamurthy <linux.vipassana@gmail.com>
2017-09-13 12:17:43 -0700

    new file:   nyc_traffic/logstash/dockerfile 	new file:   nyc_traffic/logstash/dockerfile.orig 	new file:   nyc_traffic/logstash/dummylogfile 	new file:   nyc_traffic/logstash/elk_demo2.0.json.old 	new file:   nyc_traffic/logstash/entrypoint.sh 	renamed:    nyc_traffic/logstash.conf.old -> nyc_traffic/logstash/logstash.conf.old 	renamed:    nyc_traffic/logstash.yml -> nyc_traffic/logstash/logstash.yml 	renamed:    nyc_traffic/logstash.yml.old -> nyc_traffic/logstash/logstash.yml.old 	new file:   nyc_traffic/logstash/testscript

---
+++ nyc_traffic/logstash/entrypoint.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+#set -e
+
+echo "Present Working Directory " $(pwd)
+echo "Logstash Home Directory " ${LOGSTASH_HOME}
+echo "Instruction Set URL " ${INSTR_URL}
+
+JSON_FILE="https://raw.githubusercontent.com/ao-demo/demo2.0/master/nyc_traffic/InstructionSet.json"
+# ENV variable {INSTR_URL}
+
+
+#INSTR_URL is being passed as an ENV variable from the application blueprint
+if [ -n "$INSTR_URL" ] ; then JSON_FILE=${INSTR_URL}; fi 
+
+for i in "$@"
+do
+	case $i in
+		-instr=*|--instruction-set=*)
+			JSON_FILE="${i#*=}"
+			shift
+			;;
+		-conf=*|--logstash-conf=*)
+			LOGSTASH_FILE="${i#*=}"
+			shift
+			;;
+	esac
+done
+
+# Change to WORKPATH
+cd ${WORKPATH}
+
+wget ${JSON_FILE} -O elk_demo2.0.json #renamed to elk_demo2.0.json as elkinitializer uses this name as the metafile
+./elkinitializer 
+
+# No error checking!!!. Need to improve script for the next iteration.
+# Inject Dataset into Elasticsearch
+cat dataset.csv | ${LOGSTASH_HOME}/bin/logstash -f logstash.conf
+tail -f dummylogfile
